2e813fc5ce3bce6a3c3d6b4f8dc41f44f132fab4
IgorLevch <igorlevvv@gmail.com>
2024-04-19 03:01:41 +0300

    Commit1

---
+++ src/main/java/ru/geekbraines/jwt_hometask/JwtHometaskApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbraines.jwt_hometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtHometaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtHometaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbraines/jwt_hometask/JwtHometaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbraines.jwt_hometask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtHometaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
